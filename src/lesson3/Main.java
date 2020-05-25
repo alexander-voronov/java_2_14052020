492bbd9d4291b071ba7ac0b4413076562a2551e5
Alexander Voronov <dr.strum@gmail.com>
2020-05-25 15:02:27 +0300

    HW_25.05.2020

---
+++ src/lesson3/Main.java
@@ -0,0 +1,12 @@
+package lesson3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>();
+        stringList.addAll(Arrays.asList("торт", "дом", "кошка", "кофе", "чай", "стул", "стол", "торт", "чашка", "ложка", "торт", "кошка", "окно"));
+    }
+}
