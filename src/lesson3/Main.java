a30d9e6ec554ddcbe11e07002a64f12d51bde7c8
Alexander Voronov <dr.strum@gmail.com>
2020-05-26 21:03:37 +0300

    HW_26.05.2020_lesson_3

--- src/lesson3/Main.java
+++ src/lesson3/Main.java
@@ -2,11 +2,28 @@ package lesson3;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<String> stringList = new ArrayList<>();
         stringList.addAll(Arrays.asList("торт", "дом", "кошка", "кофе", "чай", "стул", "стол", "торт", "чашка", "ложка", "торт", "кошка", "окно"));
+        System.out.println(stringList);
+
+        HashMap<String, Integer> hmap = new HashMap<>();
+        for (String a : stringList) {
+            hmap.put(a, hmap.getOrDefault(a, 0) + 1);
+        }
+        System.out.println(hmap);
+
+        Phone iBook = new Phone();
+        iBook.add("Пушкин", 123456789L);
+        iBook.add("Лермонтов", 987456123L);
+        iBook.add("Некрасов", 147852369L);
+        iBook.add("Пушкин", 369852147L);
+        iBook.add("Гоголь", 159753654852L);
+
+        System.out.println("Телефон №: " + iBook.get("Пушкин"));
     }
 }
