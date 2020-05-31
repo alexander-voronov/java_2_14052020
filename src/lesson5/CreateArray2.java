d1e724b93cfc9325e00d9050e88b3a0b3d80c5b4
Alexander Voronov <dr.strum@gmail.com>
2020-05-31 21:00:29 +0300

    HW_31.05.2020_lesson_5

---
+++ src/lesson5/CreateArray2.java
@@ -0,0 +1,65 @@
+package lesson5;
+
+public class CreateArray2 {
+
+
+    static void array() {
+
+        final int size = 10000000;
+        final int h = size / 2;
+        float[] arr = new float[size];
+
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1.0f;
+        }
+
+
+        float[] arr1 = new float[h];
+        float[] arr2 = new float[h];
+        System.arraycopy(arr, 0, arr1, 0, h);
+        System.arraycopy(arr, h, arr2, 0, h);
+
+
+        long a = System.currentTimeMillis();
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public synchronized void run() {
+                for (int i = 0; i < h; i++) {
+                    arr1[i] = (float) (arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public synchronized void run() {
+                for (int i = 0; i < h; i++) {
+                    arr2[i] = (float) (arr2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        }
+        );
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.arraycopy(arr1, 0, arr, 0, h);
+        System.arraycopy(arr2, 0, arr, h, h);
+
+        System.currentTimeMillis();
+
+        System.out.println(System.currentTimeMillis() - a);
+
+
+
+    }
+}
